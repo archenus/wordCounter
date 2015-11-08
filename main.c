ccc16120492f5ca71c45ee4d5892a26d7abe0d59
jsong <archenus@gmail.com>
2015-11-08 21:07:35 +0900

    new function

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
+void say_hello() {
+  printf("hello\n");
+}
+
 int main(int argc, char **argv){
 	char* hw = "hello world";
 	int i  = 0;
 	for(i = 0; i < 10; i++) {
 		printf("%s\n", hw);
 		printf("dongil");
-
-
 	}
 
 	return 0;
